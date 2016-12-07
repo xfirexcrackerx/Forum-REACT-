a8286f5fc24d45cd1d3022746381a39e08aa893d
Kristina <xfirexcrackerx@gmail.com>
2016-12-07 03:21:37 +0200

    some stufff :D:D
    in a hurry

--- forum/src/Components/post/allPostsView.js
+++ forum/src/Components/post/allPostsView.js
@@ -3,25 +3,57 @@ import PostComponent from './postComponent';
 import '../../Css/post/allPostsView.css';
 
 export default class postView extends Component {
-    render() {    
+    render() {
         let posts = [
         {
-            title: 'ВЪТРЕШНО ПЪРВЕНСТВО, КУПА',
-            text: 'Lorem Ipsum е елементарен примерен текст, използван в печатарската и типографската индустрия. Lorem Ipsum е индустриален стандарт от около 1500 година, когато неизвестен печатар взема няколко печатарски букви и ги разбърква, за да напечата с тях книга с примерни шрифтове. Този начин не само е оцелял повече от 5 века, но е навлязъл и в публикуването на електронни издания като е запазен почти без промяна. Популяризиран е през 60те години на 20ти век със издаването на Letraset листи, съдържащи Lorem Ipsum пасажи, популярен е и в наши дни във софтуер за печатни издания като Aldus PageMaker, който включва различни версии на Lorem Ipsum.',
-            author: 'vasko',
-            answers: 2,
-            views: 0,
+            title: 'ПРАВИЛА НА ФОРУМА',
+            text: 'Пишейки в този форум,вие се съгласявате с правилата в него',
+            author: 'ivo',
+            answers: 1,
+            views: 1046,
             id:10
         },
         {
+            title: 'ВЪТРЕШНО ПЪРВЕНСТВО, КУПА',
+            text: 'Представянето на ЛЕВСКИ в Първенството и Турнира за Купата на България',
+            author: 'vasko',
+            answers: 2,
+            views: 5000,
+            id:11
+        },
+        {
             title: 'ЕВРОПЕЙСКИ ТУРНИРИ',
-            text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
+            text: 'представянето на ЛЕВСКИ в Европейските турнири',
             author: 'krisi',
-            answers: 1,
-            views: 0,
-            id:20
-        }];
-        
+            answers: 35,
+            views: 9460,
+            id:12
+        },
+        {
+            title: 'тръст "СИНЯ БЪЛГАРИЯ"',
+            text: 'всичко свързано с тръста',
+            author: 'vasko',
+            answers: 5,
+            views: 5060,
+            id:13
+        },
+        {
+            title: 'ДРУГИ',
+            text: 'Всичко около ЛЕВСКИ, феновете и нещата свързани с тях.',
+            author: 'krisi',
+            answers: 83,
+            views: 6990,
+            id:14
+        },
+        {
+           title: 'ПРЕДСТАВИТЕЛЕН ОТБОР',
+           text: 'всичко свързано с тръста',
+           author: 'vasko',
+           answers: 24,
+           views: 1500,
+           id:15
+        }
+        ];
         for(let post in posts){
             if(posts[post].text.length > 100){
                 posts[post].text = posts[post].text.slice(0, 90) + "..."; 
@@ -54,6 +86,8 @@ export default class postView extends Component {
                     />
                 })}
             </div>
+
         )        
     }
+
 }
